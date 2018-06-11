b9486666e45600d94c4281f7ba3e9584e5746315
cincinnatus <c.cincinnat@gmail.com>
2018-06-11 18:48:59 +0300

    add python skeleton

---
+++ py/main.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import sys
+import argparse
+import signal
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='description',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    args = parser.parse_args()
+
+    try:
+        main(args)
+    except BrokenPipeError:
+        sys.exit(128 + signal.SIGPIPE)
+    except KeyboardInterrupt:
+        sys.exit(128 + signal.SIGINT)
