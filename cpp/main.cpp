15232ef7bcd03b13ace8ff1e35fd65c9a4894582
Mikhail Nozdrachev <m.nozdrachev@semrush.com>
2019-04-22 15:08:57 +0300

    cpp: handle boost program_options' exceptions

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,20 +4,26 @@
 
 int main(int argc, char *argv[])
 {
-    namespace po = boost::program_options;
+    try {
+        namespace po = boost::program_options;
 
-    po::options_description desc("allowed options");
-    desc.add_options()
-        ("help,h", "print help message and exit")
-    ;
+        po::options_description desc("allowed options");
+        desc.add_options()
+            ("help,h", "print help message and exit")
+        ;
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
 
-    if (vm.count("help")) {
-        std::cout << desc << std::endl;
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
+            return 1;
+        }
+        po::notify(vm);
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
         return 1;
     }
+
     return 0;
 }
