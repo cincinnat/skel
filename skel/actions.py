19a30fad283f1d83229a42bb166f63e120eb9294
cincinnatus <c.cincinnat@gmail.com>
2020-03-10 22:49:09 +0300

    implement skel actions

--- skel/actions.py
+++ skel/actions.py
@@ -1,15 +1,31 @@
-
-def gen_cpp_project(name):
-    raise NotImplementedError('gen_cpp_project')
-
-
-def gen_cpp_module(name):
-    raise NotImplementedError('gen_cpp_module')
+import os
+from . import file_utils
 
 
-def gen_py_project(name):
+_templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+
+def gen_cpp_project(path):
+    template = os.path.join(_templates_dir, 'cpp', 'project')
+
+    os.mkdir(path)
+    name = os.path.basename(path)
+    file_utils.copy(template, path, {'project': name})
+
+
+def gen_cpp_module(path):
+    template = os.path.join(_templates_dir, 'cpp', 'module')
+
+    dirname, name = os.path.split(path)
+    file_utils.copy(template, dirname, {'module': name})
+
+
+def gen_py_project(path):
     raise NotImplementedError('gen_py_module')
 
 
-def gen_py_script(name):
-    raise NotImplementedError('gen_py_script')
+def gen_py_script(path):
+    template = os.path.join(_templates_dir, 'py', 'script')
+
+    dirname, name = os.path.split(path)
+    file_utils.copy(template, dirname, {'script': name})
---
+++ tests/test_actions.py
@@ -0,0 +1,26 @@
+import os
+
+# pylint: disable=unused-wildcard-import,wildcard-import
+from skel.actions import *
+
+
+def test_gen_cpp_project(tmpdir):
+    path = os.path.join(tmpdir, 'proj')
+    gen_cpp_project(path)
+
+    assert os.path.exists(os.path.join(path, 'CMakeLists.txt'))
+
+
+def test_gen_cpp_module(tmpdir):
+    path = os.path.join(tmpdir, 'mod')
+    gen_cpp_module(path)
+
+    assert os.path.exists(path + '.cpp')
+    assert os.path.exists(path + '.h')
+
+
+def test_gen_py_module(tmpdir):
+    path = os.path.join(tmpdir, 'script')
+    gen_py_script(path)
+
+    assert os.path.exists(path + '.py')
